26bdd200d853452afc64941f7cce57aac09bcaf0
Sebastien Griffoul <sebastien.griffoul@intel.com>
2014-12-12 10:45:39 +0100

    Bluetooth-LE: Add API to get the handle assigned to a GATT service
    Some BLE Service profiles require to broadcast a service handle
    as part of the service data during advertisment.
    
    It's the case of the WPT service specified by A4WP (for Wireless
    charging).
    
    Unfortunately this handle is assigned at the stack level and the
    application doesn't have access to it when building advertisment
    data.
    
    That's why a new API has been added to the GATT server:
        public int getServiceHandle()
    
    Calling this function allows the application to know about the
    handle which has been assigned for its BLE Service and to use
    it when building advertisment.
    
    This patch has a dependency on the frameworks/base
    
    Change-Id: Ibeda39799422bdce572d6286f3fa4a6e76091f3b
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-13491
    Category: aosp improvement
    Domain: CWS_BT-BLE
    Origin: internal
    Upstream-Candidate: yes
    Signed-off-by: Sebastien Griffoul <sebastien.griffoul@intel.com>
    Reviewed-on: https://android.intel.com:443/311249

--- src/com/android/bluetooth/gatt/GattService.java
+++ src/com/android/bluetooth/gatt/GattService.java
@@ -537,6 +537,13 @@ public class GattService extends ProfileService {
             if (service == null) return;
             service.stopMultiAdvertising(new AdvertiseClient(clientIf));
         }
+
+        public int getServiceHandle(int srvcType, int srvcInstanceId,
+                ParcelUuid srvcId) {
+            GattService service = getService();
+            if (service == null) return 0;
+            return service.getServiceHandle(srvcType, srvcInstanceId, srvcId.getUuid());
+        }
     };
 
     /**************************************************************************
@@ -1970,6 +1977,11 @@ public class GattService extends ProfileService {
         }
     }
 
+    int getServiceHandle(int srvcType, int srvcInstanceId, UUID srvcUuid) {
+        enforceCallingOrSelfPermission(BLUETOOTH_PERM, "Need BLUETOOTH permission");
+
+        return mHandleMap.getServiceHandle(srvcUuid, srvcType, srvcInstanceId);
+    }
 
     /**************************************************************************
      * Private functions
