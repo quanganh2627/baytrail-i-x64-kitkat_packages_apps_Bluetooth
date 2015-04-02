a341799d3f900d8427bb74b8b68a6a2ba41378c7
Zhang Wei <wei.e.zhang@intel.com>
2015-04-02 11:47:25 +0000

    [PATCH] [Sofia Lpop]: OPP throughput improvement
    BZ: IMINAN-28513
    
    Progress Bar updation during OPP takes around 40ms for each update.
    This causes a significant delay in OPP file transfer, which eventually
    will decrease the throughput.
    To improve the throughput, invoke the progressBar updation only if there is a
    change in the percentage.
    CherryPick from https://android.intel.com/#/c/349679/
    
    Change-Id: If23eface1300ff33a7de96ceced42ce24bfff0c4
    Signed-off-by: Zhang Wei <wei.e.zhang@intel.com>
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-28513

--- src/com/android/bluetooth/opp/BluetoothOppObexClientSession.java
+++ src/com/android/bluetooth/opp/BluetoothOppObexClientSession.java
@@ -393,6 +393,8 @@ public class BluetoothOppObexClientSession implements BluetoothOppObexSession {
                 if (!error) {
                     int position = 0;
                     int readLength = 0;
+                    long percent = 0;
+                    long prevPercent = 0;
                     boolean okToProceed = false;
                     long timestamp = 0;
                     int outputBufferSize = putOperation.getMaxPacketSize();
@@ -465,10 +467,15 @@ public class BluetoothOppObexClientSession implements BluetoothOppObexSession {
                                             + " readLength " + readLength + " bytes took "
                                             + (System.currentTimeMillis() - timestamp) + " ms");
                                 }
-                                updateValues = new ContentValues();
-                                updateValues.put(BluetoothShare.CURRENT_BYTES, position);
-                                mContext1.getContentResolver().update(contentUri, updateValues,
-                                        null, null);
+                                // Update the Progress Bar only if there is change in percentage
+                                percent = position * 100 / fileInfo.mLength;
+                                if (percent > prevPercent) {
+                                    updateValues = new ContentValues();
+                                    updateValues.put(BluetoothShare.CURRENT_BYTES, position);
+                                    mContext1.getContentResolver().update(contentUri, updateValues,
+                                            null, null);
+                                    prevPercent = percent;
+                                }
                             }
                         }
                     }
